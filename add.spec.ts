7889e9f87805c7af2668ef84386fea28c95acb9c
Charles-Étienne Joseph <charles-etienne.joseph@polymtl.ca>
2019-01-11 11:06:42 -0500

    added tests 1a and 1b

--- add.spec.ts
+++ add.spec.ts
@@ -6,4 +6,10 @@ describe('add function', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).to.equal(0);
     });
+    it('should return n for a unique number n', () => {
+        expect(add('2')).to.equal(2);
+    });
+    it('should return n for a unique number n', () => {
+        expect(add('14')).to.equal(14);
+    });
 });

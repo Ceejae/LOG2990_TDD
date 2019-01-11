d588fd707685404d51fc46b3229d61b93ef51295
Charles-Étienne Joseph <charles-etienne.joseph@polymtl.ca>
2019-01-11 11:16:55 -0500

    function add passes tests 1a and 1b

--- add.ts
+++ add.ts
@@ -1,3 +1,14 @@
 export function add(s: string): number {
+
+    if(s.length == 0) {
+        return 0;
+    }
+
+    let numbers = s.split(',');
+    if(numbers.length == 1) {
+        return parseInt(numbers[0]);
+    }
+
     return 0;
+
 }
